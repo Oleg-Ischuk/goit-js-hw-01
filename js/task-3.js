989a0376f2296d236f0eca23639fdc0c59ff9780
Oleg_Ischuk <ipz231_ios@student.ztu.edu.ua>
2024-10-30 19:57:58 +0200

    changes

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,4 @@
-function getElementWidth(content,border,padding) {
+function getElementWidth(content,padding,border) {
     const contentWidth = parseFloat(content);
     const paddingWidth = parseFloat(padding) * 2;
     const borderWidth = parseFloat(border) * 2;
